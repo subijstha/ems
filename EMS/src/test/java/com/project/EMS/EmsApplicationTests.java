1b23688ca4ea4e407d2e698981ddcdcf6228e99f
Subij Shrestha <36372614+subijstha@users.noreply.github.com>
2023-08-28 14:32:46 -0500

    initial

---
+++ EMS/src/test/java/com/project/EMS/EmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.EMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
