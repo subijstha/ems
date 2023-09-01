0163ad2649f7876bd989e50492c7223cecd8697a
Subij Shrestha <36372614+subijstha@users.noreply.github.com>
2023-09-01 15:13:50 -0500

    createEmployee backend

--- EMS/src/main/java/com/project/EMS/Service/EmployeeDTO.java
+++ EMS/src/main/java/com/project/EMS/Service/EmployeeDTO.java
@@ -11,3 +11,4 @@ public class EmployeeDTO {
     private String emailId;
 
 }
+
--- EMS/src/main/java/com/project/EMS/controller/EmployeeController.java
+++ EMS/src/main/java/com/project/EMS/controller/EmployeeController.java
@@ -2,14 +2,13 @@ package com.project.EMS.controller;
 
 
 import com.project.EMS.Repository.EmployeeRepository;
+import com.project.EMS.Service.EmployeeDTO;
 import com.project.EMS.model.Employee;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,9 +19,24 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
     @GetMapping("/employees")
     public ResponseEntity<?>  getAllEmployees(){
         List<Employee> employees = employeeRepository.findAll();
         return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @PostMapping("/employees")
+    public ResponseEntity<?> createEmployee(@RequestBody EmployeeDTO employeeDTO){
+
+        Employee existingEmployee = employeeRepository.findByEmailId(employeeDTO.getEmailId());
+        if(existingEmployee != null){
+            return new ResponseEntity<>("Employee with the same email already exists ", HttpStatus.BAD_REQUEST);
+        }
+        Employee employee = modelMapper.map(employeeDTO, Employee.class);
+        employeeRepository.save(employee);
+
+        return new ResponseEntity<Employee>(employee, HttpStatus.CREATED);
     }
 }
