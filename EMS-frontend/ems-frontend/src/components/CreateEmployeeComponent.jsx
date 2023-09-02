da2dbf207dd7fc5fc63ed0ee2b5e1a5ecb52f298
Subij Shrestha <36372614+subijstha@users.noreply.github.com>
2023-09-01 20:56:53 -0500

    Single page update and add

--- EMS-frontend/ems-frontend/src/components/CreateEmployeeComponent.jsx
+++ EMS-frontend/ems-frontend/src/components/CreateEmployeeComponent.jsx
@@ -1,3 +1,146 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import EmployeeService from '../services/EmployeeService';
+
+function CreateEmployeeComponent() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const [employees, setEmployees] = useState([]);
+  const [employee, setEmployee] = useState({
+    firstName: '',
+    lastName: '',
+    emailId: '',
+  });
+
+  useEffect(() => {
+    EmployeeService.getEmployees().then((res) => {
+      const sortedEmployees = res.data.sort((a, b) => a.id - b.id);
+      setEmployees(sortedEmployees);
+    });
+
+    if (id === '_add') {
+      // When adding a new employee, initialize state with empty values
+      setEmployee({
+        firstName: '',
+        lastName: '',
+        emailId: '',
+      });
+    } else {
+      // Fetch the existing employee data using the ID
+      EmployeeService.getEmployeeById(id).then((res) => {
+        const employeeData = res.data;
+        setEmployee(employeeData);
+      });
+    }
+  }, [id]);
+
+  const saveOrUpdateEmployee = (e) => {
+    e.preventDefault();
+
+    const { firstName, lastName, emailId } = employee;
+    const newEmployee = { firstName, lastName, emailId };
+
+    if (id === '_add') {
+      // Adding a new employee
+      EmployeeService.createEmployee(newEmployee).then((res) => {
+        navigate('/employees');
+      });
+    } else {
+      // Updating an existing employee
+      EmployeeService.updateEmployee(newEmployee, id).then((res) => {
+        navigate('/employees');
+      });
+    }
+  };
+
+  const cancel = () => {
+    navigate('/employees');
+  };
+
+  const getTitle = () => {
+    if (id === '_add') {
+      return <h3 className="text-center">Add Employee</h3>;
+    } else {
+      return <h3 className="text-center">Update Employee</h3>;
+    }
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="card col-md-6 offset-md-3 offset-md-3">
+            {getTitle()}
+
+            <div className="card-body">
+              <form>
+                <div className="form-group">
+                  <label>First Name</label>
+                  <input
+                    placeholder="First Name"
+                    name="firstName"
+                    className="form-control"
+                    value={employee.firstName}
+                    onChange={(e) =>
+                      setEmployee({ ...employee, firstName: e.target.value })
+                    }
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label>Last Name</label>
+                  <input
+                    placeholder="Last Name"
+                    name="lastName"
+                    className="form-control"
+                    value={employee.lastName}
+                    onChange={(e) =>
+                      setEmployee({ ...employee, lastName: e.target.value })
+                    }
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label>Email</label>
+                  <input
+                    placeholder="Email Address"
+                    name="emailId"
+                    className="form-control"
+                    value={employee.emailId}
+                    onChange={(e) =>
+                      setEmployee({ ...employee, emailId: e.target.value })
+                    }
+                  />
+                </div>
+
+                <button
+                  className="btn btn-success"
+                  onClick={saveOrUpdateEmployee}
+                >
+                  {id === '_add' ? 'Save' : 'Update'}
+                </button>
+
+                <button
+                  className="btn btn-danger"
+                  onClick={cancel}
+                  style={{ marginLeft: '10px' }}
+                >
+                  Cancel
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateEmployeeComponent;
+
+
+
 // import React, { Component } from 'react';
 // import { withRouter } from 'react-router-dom';
 
@@ -191,144 +334,5 @@
 // export default CreateEmployeeComponent;
 
 
-import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import EmployeeService from '../services/EmployeeService';
 
-function CreateEmployeeComponent() {
-  const navigate = useNavigate();
-  const { id } = useParams();
-
-  const [employees, setEmployees] = useState([]);
-  const [employee, setEmployee] = useState({
-    firstName: '',
-    lastName: '',
-    emailId: '',
-  });
-
-  useEffect(() => {
-    EmployeeService.getEmployees().then((res) => {
-      const sortedEmployees = res.data.sort((a, b) => a.id - b.id);
-      setEmployees(sortedEmployees);
-    });
-
-    if (id === '_add') {
-      // When adding a new employee, initialize state with empty values
-      setEmployee({
-        firstName: '',
-        lastName: '',
-        emailId: '',
-      });
-    } else {
-      // Fetch the existing employee data using the ID
-      EmployeeService.getEmployeeById(id).then((res) => {
-        const employeeData = res.data;
-        setEmployee(employeeData);
-      });
-    }
-  }, [id]);
-
-  const saveOrUpdateEmployee = (e) => {
-    e.preventDefault();
-
-    const { firstName, lastName, emailId } = employee;
-    const newEmployee = { firstName, lastName, emailId };
-
-    if (id === '_add') {
-      // Adding a new employee
-      EmployeeService.createEmployee(newEmployee).then((res) => {
-        navigate('/employees');
-      });
-    } else {
-      // Updating an existing employee
-      EmployeeService.updateEmployee(newEmployee, id).then((res) => {
-        navigate('/employees');
-      });
-    }
-  };
-
-  const cancel = () => {
-    navigate('/employees');
-  };
-
-  const getTitle = () => {
-    if (id === '_add') {
-      return <h3 className="text-center">Add Employee</h3>;
-    } else {
-      return <h3 className="text-center">Update Employee</h3>;
-    }
-  };
-
-  return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <div className="card col-md-6 offset-md-3 offset-md-3">
-            {getTitle()}
-
-            <div className="card-body">
-              <form>
-                <div className="form-group">
-                  <label>First Name</label>
-                  <input
-                    placeholder="First Name"
-                    name="firstName"
-                    className="form-control"
-                    value={employee.firstName}
-                    onChange={(e) =>
-                      setEmployee({ ...employee, firstName: e.target.value })
-                    }
-                  />
-                </div>
-
-                <div className="form-group">
-                  <label>Last Name</label>
-                  <input
-                    placeholder="Last Name"
-                    name="lastName"
-                    className="form-control"
-                    value={employee.lastName}
-                    onChange={(e) =>
-                      setEmployee({ ...employee, lastName: e.target.value })
-                    }
-                  />
-                </div>
-
-                <div className="form-group">
-                  <label>Email</label>
-                  <input
-                    placeholder="Email Address"
-                    name="emailId"
-                    className="form-control"
-                    value={employee.emailId}
-                    onChange={(e) =>
-                      setEmployee({ ...employee, emailId: e.target.value })
-                    }
-                  />
-                </div>
-
-                <button
-                  className="btn btn-success"
-                  onClick={saveOrUpdateEmployee}
-                >
-                  {id === '_add' ? 'Save' : 'Update'}
-                </button>
-
-                <button
-                  className="btn btn-danger"
-                  onClick={cancel}
-                  style={{ marginLeft: '10px' }}
-                >
-                  Cancel
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default CreateEmployeeComponent;
 
